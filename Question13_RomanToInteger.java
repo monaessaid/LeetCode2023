b9e61daf0fa29d04ce1fae15d3f305c1923d030d
Mona <2829922e@student.gla.ac.uk>
2023-05-22 15:15:51 -0700

    RomanToArabic

--- Question13_RomanToInteger.java
+++ Question13_RomanToInteger.java
@@ -18,7 +18,7 @@
 //			if (chars[i] == 'I' && chars[j] == 'V') {
 //			int k = 5;
 
-//I think we have to convert them and add them seperately
+//I think we have to convert them and add them separately
 //For (items in char array) if a[i] = 'I', copy it into a separate int array with value 1
 //Same if statement as before, if i and j are iv, add it to the array as 4
 //Then do ANOTHER loop and add everything in the int array 
@@ -39,40 +39,74 @@ public class Question13_RomanToInteger {
 		// Then add it to the regular loop sum?
 
 		for (int i = 0; i < n; i++) {
-			int j = i + 1;
-			if (chars[i] == 'I' && chars[j] == 'V') {
-				sum = sum + 4;
-				break;
-			} else if (chars[i] == 'I' && chars[j] == 'X') {
-				sum = sum + 9;
-				break;
-			} else if (chars[i] == 'X' && chars[j] == 'L') {
-				sum = sum + 40;
-				break;
-			} else if (chars[i] == 'X' && chars[j] == 'C') {
-				sum = sum + 90;
-				break;
-			} else if (chars[i] == 'C' && chars[j] == 'D') {
-				sum = sum + 400;
-				break;
-			} else if (chars[i] == 'C' && chars[j] == 'M') {
-				sum = sum + 900;
-				break;
-			} else {
-				return -1;
+			// Gets rid of the array out of bounds exception
+			// Runs ONLY if you haven't gotten to the end of the array
+			// Needed to add the i++ within these edge case loops because there are 2 scenarios:
+			// Scenario 1 is going through one by one, which is what is needed for something like 'I' by itself
+			// Scenario 2 is going through it as twos, which you need for all the edge cases, and to determine if something is an edge case
+			// What this part of the loop does is handle each edge case, and manually increment i by another 1 so we don't have to separate and combine
+			// Reason you need to increment is is becasue i is your cursor - otherwise, each edge case will only iterate your "cursor" by 1, which will double count j
+			if (i < n - 1) {
+				int j = i + 1;
+				if (chars[i] == 'I' && chars[j] == 'V') {
+					sum = sum + 4;
+					i++;
+					continue;
+				} else if (chars[i] == 'I' && chars[j] == 'X') {
+					sum = sum + 9;
+					i++;
+					continue;
+				} else if (chars[i] == 'X' && chars[j] == 'L') {
+					sum = sum + 40;
+					i++;
+					continue;
+				} else if (chars[i] == 'X' && chars[j] == 'C') {
+					sum = sum + 90;
+					i++;
+					continue;
+				} else if (chars[i] == 'C' && chars[j] == 'D') {
+					sum = sum + 400;
+					i++;
+					continue;
+				} else if (chars[i] == 'C' && chars[j] == 'M') {
+					sum = sum + 900;
+					i++;
+					continue;
+				}
+			}
+			if (chars[i] == 'I') {
+				sum = sum + 1;
+				continue;
+			} else if (chars[i] == 'V') {
+				sum = sum + 5;
+				continue;
+			} else if (chars[i] == 'X') {
+				sum = sum + 10;
+				continue;
+			} else if (chars[i] == 'L') {
+				sum = sum + 50;
+				continue;
+			} else if (chars[i] == 'C') {
+				sum = sum + 100;
+				continue;
+			} else if (chars[i] == 'D') {
+				sum = sum + 500;
+				continue;
+			} else if (chars[i] == 'M') {
+				sum = sum + 1000;
+				continue;
 			}
 		}
 		return sum;
 	}
 
 	public static void main(String[] args) {
-//		System.out.println(romanToInt("I"));
-//		System.out.println(romanToInt("II"));
-//		System.out.println(romanToInt("III"));
-//		System.out.println(romanToInt("IIII"));
-//		System.out.println(romanToInt("IV"));
-//		System.out.println(romanToInt("V"));
-//		System.out.println(romanToInt("VI"));
+		System.out.println(romanToInt("I"));
+		System.out.println(romanToInt("II"));
+		System.out.println(romanToInt("III"));
+		System.out.println(romanToInt("IV"));
+		System.out.println(romanToInt("V"));
+		System.out.println(romanToInt("VI"));
 
 		// IV (4), IX (9), XL (40), XC (90), CD (400), CM (900)
 
@@ -83,15 +117,9 @@ public class Question13_RomanToInteger {
 		System.out.println(romanToInt("CD"));
 		System.out.println(romanToInt("CM"));
 		System.out.println(romanToInt("CMIV"));
+		System.out.println(romanToInt("MCMXCIV"));
+		System.out.println(romanToInt("LVIII"));
 
 	}
 
 }
-
-//} else if (chars[i] == 'I') {
-//	sum = sum + 1;
-//	break;
-//} else if (chars[i] == 'V') {
-//	sum = sum + 5;
-//	break;
-//}
