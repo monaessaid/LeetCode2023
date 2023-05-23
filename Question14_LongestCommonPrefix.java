58b0f420684a82f33a8148a5a258f2c8b8c85bc9
Mona <2829922e@student.gla.ac.uk>
2023-05-23 01:15:58 -0700

    LongestCommonPrefix

---
+++ Question14_LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+// Initial thoughts:
+// Step 1: Separate the strings into words based on the spaces
+// Step 2: Use String.equals() to see if the strings are the same, if so, return the character count
+// * Not sure how practical step 2 is since strs.length can be up to 200g
+// Step 3: Iterate through each and compare character by character 
+// Step 4: Print the characters from the 2 strings that are equal
+// Step 5: Else condition should be "" for no common prefix
+
+
+public class Question14_LongestCommonPrefix {
+
+	public String longestCommonPrefix(String[] strs) {
+		
+		
+
+	}
+
+	public static void main(String[] args) {
+
+		String[] strs = new String[] { "flower", "flow", "flight" };
+
+	}
+
+}
